b102016766b192b84ceeb0bec6653a3b1352a6c0
Lubotech <luboyerajoel573@gmail.com>
2024-09-19 19:49:44 +0300

    Worked on Numeric data types

--- Luboyera_Joel_Tut1/Program.cs
+++ Luboyera_Joel_Tut1/Program.cs
@@ -10,6 +10,44 @@ namespace Luboyera_Joel_Tut1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to C#");
+
+            //int x = 10;
+            //int y = 20; 
+            //int z = 30;
+
+            int x = 10, 
+                y = 20, 
+                z = 30;
+
+            int age = -28;
+            Console.WriteLine(age);
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(int.MaxValue);
+
+            long bigNumber = -9000000000L;
+            Console.WriteLine(bigNumber);
+            Console.WriteLine(long.MinValue);
+            Console.WriteLine(long.MaxValue);
+
+            double negative = -58.56D;
+            Console.WriteLine(negative);
+            Console.WriteLine(double.MaxValue);
+            Console.WriteLine(double.MinValue);
+
+            float precision = 6.768570F;
+            Console.WriteLine(precision);
+            Console.WriteLine(float.MinValue);
+            Console.WriteLine(float.MaxValue);
+
+            decimal money = 14.99M;
+            Console.WriteLine(money);
+            Console.WriteLine(decimal.MinValue);
+            Console.WriteLine(decimal.MaxValue);
+
+            Console.ReadLine();
+
+
         }
     }
 }
