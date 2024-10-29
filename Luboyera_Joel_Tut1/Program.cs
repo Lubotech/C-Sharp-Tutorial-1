a4f327302331d9038cb856f76356e4a832c02e79
Lubotech <luboyerajoel573@gmail.com>
2024-10-29 08:19:19 +0300

    Did some work here

--- Luboyera_Joel_Tut1/Program.cs
+++ Luboyera_Joel_Tut1/Program.cs
@@ -21,7 +21,7 @@ namespace Luboyera_Joel_Tut1
                 z = 30;
 
             string textAge = "-27";
-            int age = Convert.ToInt32(textAge);
+            var age = Convert.ToInt32(textAge);
             Console.WriteLine(age);
             Console.WriteLine(int.MinValue);
             Console.WriteLine(int.MaxValue);
@@ -50,7 +50,7 @@ namespace Luboyera_Joel_Tut1
 
             Console.ReadLine();
 
-            Console.WriteLine("What is Your Name");
+            Console.WriteLine("Here we are");
             string name = Console.ReadLine();
             Console.WriteLine(name + " Welcome to our lesson");
             Console.ReadKey();
@@ -68,13 +68,107 @@ namespace Luboyera_Joel_Tut1
             Console.WriteLine(num);
 
             string name2 = "Yana";
-            name2 += " is a programmer";
+
+            name2 += "is a programmer";
             Console.WriteLine(name2);
 
-            char letter2 = 'A';
-            letter2 += 'B';
-            Console.WriteLine('A');
+            char letter2 = 'a';
+            letter2 += 'b';
+            Console.WriteLine(letter2);
 
+            float i = 2; i /= 3;
+            Console.WriteLine(++i);
+
+            const int vat = 20;
+            const double percentVAT = (20 / 100D);
+
+            int balance = 1000;
+
+            Console.WriteLine(balance * (20 / 100D));
+            Console.WriteLine(balance * percentVAT);
+
+            Console.Write("Enter your name: ");
+            string name3 = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            var age1Input = Console.ReadLine();
+            int age1 = Convert.ToInt32(age1Input);
+
+            Console.WriteLine("Your name is " + name3 + " and your age " + age1);
+
+            // ==  >  >=  <  <=  !=
+
+
+            if (age1 < 0 || age1 > 150)
+            {
+                Console.WriteLine("Invalid age!");
+            }
+            else
+            {
+                if (age1 >= 18 && age <= 25)
+                {
+                    Console.WriteLine("You are between 18 and 25");
+                }
+                else if (age1 >= 26)
+                {
+                    Console.WriteLine("You are 26 or older");
+                }
+                
+            }
+
+            Console.Write("Enter the first number: ");
+            int numberA = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            int numberB = Convert.ToInt32(Console.ReadLine());
+
+            int answer = numberA * numberB;
+
+            Console.Write("Value of " + numberA + " X " + numberB + ": ");
+            int actualAnswer = Convert.ToInt32(Console.ReadLine());
+
+            if (answer == actualAnswer)
+            {
+                Console.WriteLine("Well done!");
+            }
+            else if (answer != actualAnswer)
+            {
+                Console.WriteLine("Close but it was wrong!");
+            }
+
+            Console.Write("Enter a day of the week: ");
+            int day = Convert.ToInt32(Console.ReadLine());
+
+            switch (day)
+            {
+                case 1: Console.WriteLine("Mon");
+                    break;
+                case 2:
+                    Console.WriteLine("Tue");
+                    break;
+                case 3:
+                    Console.WriteLine("Wed");
+                    break;
+                case 4:
+                    Console.WriteLine("Thur");
+                    break;
+                case 5:
+                    Console.WriteLine("Fri");
+                    break;
+                case 6:
+                    Console.WriteLine("Sat");
+                    break;
+                case 7:
+                    Console.WriteLine("Sun");
+                    break;
+                default: Console.WriteLine("Invalid, enter a value between 1 and 7");
+                    break;
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+
+            }
             Console.ReadLine();
 
         }
