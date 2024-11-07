cc4557415387e6f863a19de4c9b248a7cf6ad3a7
Lubotech <luboyerajoel573@gmail.com>
2024-11-07 08:32:50 +0300

    Worked on indexOf Method

--- Luboyera_Joel_Tut1/Program.cs
+++ Luboyera_Joel_Tut1/Program.cs
@@ -566,16 +566,75 @@ namespace Luboyera_Joel_Tut1
             {
                 Console.Write($"{numz} ");
             }
-         
+
+            Console.WriteLine();
+
             Array.Reverse(numberz);
             foreach (var item in numberz)
             {
                 Console.Write($"{item} ");
             }
 
+            Console.WriteLine();
+
             Array.Clear(numberz, 0, numberz.Length);
 
             foreach (var item in numberz)
+            {
+                Console.Write($"{item} ");
+            }
+
+            Array.Clear(numberz, 5, 5);
+
+            for (int i = 5; i < 10; i++)
+            {
+                numberz[i] = default;
+            }
+
+            // Array indexOf method used to search through an array to get a position
+
+            int[] numberss = new int[] 
+            {
+                90, 199, 34, 78, 50, 20, 199, 34
+            };
+
+            Console.Write("Enter number to search: ");
+            int search = Convert.ToInt32(Console.ReadLine());
+
+
+            //int position = Array.IndexOf(numbers, search); // method for searching through everything
+            //int position = Array.IndexOf(numbers, search, 4); // method for searching upto a certain position 
+            int position = Array.IndexOf(numbers, search, 1, 5); // method for searching in a certain range
+
+            if (position > -1)
+            {
+                Console.WriteLine($"Number {search} has been found at position {position + 1}");
+            }
+            else
+            {
+                Console.WriteLine($"Number {search} has not been found");
+            }
+            
+            /*
+            int position = -1;
+
+            for (int i = 0; i < numberss.Length; i++)
+            {
+                if (numberss[i] == search)
+                {
+                    position = i;
+                }
+            }
+
+            if (position > -1)
+            {
+                Console.WriteLine($"Number {search} has been found at position {position + 1}");
+            }
+            else
+            {
+                Console.WriteLine($"Number {search} has not been found");
+            }
+            */
 
             Console.ReadLine();
 
