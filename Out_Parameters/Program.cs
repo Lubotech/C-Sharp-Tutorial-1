830451b760687c9b491f20f46e47872488497945
Luboyera Joel <luboyerajoel573@gmail.com>
2024-11-19 01:02:26 +0300

    Work on the reference parameters

--- Out_Parameters/Program.cs
+++ Out_Parameters/Program.cs
@@ -17,7 +17,62 @@ namespace Out_Parameters
             
             //int.TryParse("123", out int result);
 
+            List<string> shoppinglist = new List<string>
+            {
+                "Coffee", "Milk"
+            };
+
+            Console.WriteLine(shoppinglist.IndexOf("Milk"));
+            //Console.WriteLine(FindInList("Milk", shoppinglist, out int index));
+            //Console.Write(index);
+
+            Console.Write("Enter an item to search: ");
+            string search = Console.ReadLine();
+
+            if (FindInList(search, shoppinglist, out int index))
+            {
+                Console.WriteLine($"Found {search} at index {index}");
+            }
+            else
+            {
+                Console.WriteLine("Not found");
+            }
+
+            /*
+            int index = -1; // 0
+
+            for (int i = 0; i < shoppinglist.Count; i++)
+            {
+                if (shoppinglist[i].ToLower().Equals("coffee"))
+                {
+                    index = i; 
+                }
+            }
+            bool found = index > -1;
+            Console.WriteLine(found  ? "Found" : "Not found");
+            */
             Console.ReadLine();
+        }
+
+        static bool FindInList(string s, List<string> list, out int index)
+        {
+            index = -1; // 0
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].ToLower().Equals(s.ToLower()))
+                {
+                    index = i;
+                }
+            }
+
+            return index > -1;
+        }
+
+        static bool TryParse(string s, out int result)
+        {
+            result = 0;
+            return true;
         }
 
         static void test(out int num) 
---
+++ Reference_Parameters/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reference_Parameters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 10; 
+            Assign(out num);
+            Console.WriteLine("num");
+
+            Console.ReadLine();
+        }
+
+        static void Assign(out int num)
+        {
+            num = 20;
+        }
+    }
+}
