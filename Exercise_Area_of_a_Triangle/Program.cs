5bcde04819e6aa8bf303ecacc54cb54f02df2558
Lubotech <luboyerajoel573@gmail.com>
2024-11-19 03:50:34 +0300

    Worked on more functions with arrays

---
+++ Exercise_Area_of_a_Triangle/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Area_of_a_Triangle
+{
+    internal class Program
+    {
+        /*
+         * Ask a user for base and height, store them
+         * Create function to calculate the area
+         * Function should calculate the area using: (base x height) / 2
+         * Call in main and print out the area of the triangle
+         */
+        static void Main(string[] args)
+        {
+            
+            int width = ReadInt("Enter base: ");
+            int height = ReadInt("Enter height: ");
+
+            int result = calcArea(width, height);
+            Console.WriteLine($"The area is {result}");
+
+            Console.ReadLine();
+        }
+
+        static int calcArea(int width, int height)
+        {
+            return (width * height) / 2;
+        }
+        static int ReadInt(string message) 
+        {
+            Console.Write(message);
+            return Convert.ToInt32(Console.ReadLine());
+            
+
+
+        }
+    }
+}
--- Reference_Parameters/Program.cs
+++ Reference_Parameters/Program.cs
@@ -11,15 +11,43 @@ namespace Reference_Parameters
     {
         static void Main(string[] args)
         {
-            int num = 10; 
-            Assign(out num);
-            Console.WriteLine("num");
+            int num = 10;
+            string name = "Abraham";
+            Assign(ref num, ref name);
+            Console.WriteLine(num);
+            Console.WriteLine(name);
 
+            Console.WriteLine();
+
+            Console.Write("Enter your new name: ");
+            string newName = Console.ReadLine();
+
+            if (ChangeName(ref name, newName))
+            {
+              Console.WriteLine($"Your new name is {name}");
+            }
+            else
+            {
+                Console.WriteLine("New name can not be empty");
+            }
             Console.ReadLine();
         }
 
-        static void Assign(out int num)
+        static bool ChangeName(ref string name, string newName)
+        { 
+            if (!string.IsNullOrEmpty(newName))
+            {
+                name = newName;
+                return true;
+            }
+
+            return false;
+            
+        }
+
+        static void Assign(ref int num, ref string name)
         {
+            name = "Joel";
             num = 20;
         }
     }
